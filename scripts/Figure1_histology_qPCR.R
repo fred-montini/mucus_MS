36f753d1a992bb4d108161158e227e5db851d70a
fred-montini <feder.montini@gmail.com>
2026-05-16 13:04:13 -0500

    Update Figure1_histology.R

--- scripts/Figure1_histology_qPCR.R
+++ scripts/Figure1_histology_qPCR.R
@@ -1,4 +1,4 @@
-# Figure1_histology_qPCR.R
+# Figure1_histology.R
 
 source("scripts/00_setup.R")
 
@@ -23,23 +23,3 @@ p_histology <- ggplot(histology, aes(x = group, y = value, color = group)) +
 
 ggsave("outputs/figures/Figure1B_histology_quantification.pdf", p_histology, width = 8, height = 4)
 
-# Input: data_processed/figure1_qpcr_ddct.csv
-# Required columns: sample_id, group, gene, ddct_or_relative_expression
-qpcr <- read_csv("data_processed/figure1_qpcr_ddct.csv") %>%
-  mutate(group = factor(group, levels = c("HC", "MS")), gene = factor(gene))
-
-qpcr_stats <- qpcr %>%
-  group_by(gene) %>%
-  wilcox_test(ddct_or_relative_expression ~ group) %>%
-  add_significance("p")
-
-write_csv(qpcr_stats, "outputs/statistics/Figure1C_qPCR_MannWhitney.csv")
-
-p_qpcr <- ggplot(qpcr, aes(x = group, y = ddct_or_relative_expression, color = group)) +
-  geom_jitter(width = 0.12, size = 2, alpha = 0.8) +
-  stat_summary(fun = mean, geom = "crossbar", width = 0.4, color = "black") +
-  facet_wrap(~ gene, scales = "free_y") +
-  labs(x = NULL, y = "Relative expression", title = "Mucin gene expression") +
-  theme(legend.position = "none")
-
-ggsave("outputs/figures/Figure1C_qPCR_mucins.pdf", p_qpcr, width = 10, height = 6)
